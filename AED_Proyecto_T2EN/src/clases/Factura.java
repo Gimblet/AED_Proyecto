2469ccaa9c5a90e9d6bc74cd947d43d235533e7a
Gimblet <gimbletdev@gmail.com>
2023-11-19 19:48:52 -0500

    Clase Factura
    Se implementaron los atributos basicos de la clase factura y se inicializo el proyecto en InteliJ

--- AED_Proyecto_T2EN/src/clases/Factura.java
+++ AED_Proyecto_T2EN/src/clases/Factura.java
@@ -1,5 +1,9 @@
 package clases;
 
 public class Factura {
-	
+    private int codigoFactura = 4001;
+    private int codigoProducto;
+    private int codigoVendedor;
+    private int unidades;
+    private double precio;
 }
